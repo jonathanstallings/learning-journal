e6622f4321388b1a9a4308aed32984f04b603032
Jonathan Stallings <jonathanstallings@outlook.com>
2015-06-22 16:38:10 -0700

    Add journal.py from instructions

---
+++ journal.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import os
+from pyramid.config import Configurator
+from pyramid.view import view_config
+import sqlaclchemy as sa
+from sqlaclchemy.ext.declarative import declarative_base
+from waitress import serve
+
+
+@view_config(route_name='home', renderer='string')
+def home(request):
+    return "Hello World"
+
+
+def main():
+    """Create a configured wsgi app"""
+    settings = {}
+    debug = os.environ.get('DEBUG', True)
+    settings['reload_all'] = debug
+    settings['debug_all'] = debug
+    # configuration setup
+    config = Configurator(
+        settings=settings
+    )
+    config.add_route('home', '/')
+    config.scan()
+    app = config.make_wsgi_app()
+    return app
+
+
+if __name__ == '__main__':
+    app = main()
+    port = os.environ.get('PORT', 5000)
+    serve(app, host='0.0.0.0', port=port)
