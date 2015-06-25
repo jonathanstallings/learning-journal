cc1e2e85d86f728d2d90599c752a9d52ca3b756f
Jonathan Stallings <jonathanstallings@outlook.com>
2015-06-24 19:41:59 -0700

    Add connection pytest fixture

--- test_journal.py
+++ test_journal.py
@@ -13,3 +13,15 @@ os.environ['DATABASE_URL'] = TEST_DATABASE_URL
 os.environ['TESTING'] = "True"
 
 import journal
+
+
+@pytest.fixture(scope='session')
+def connection(request):
+    engine = create_engine(TEST_DATABASE_URL)
+    journal.Base.metadata.create_all(engine)
+    connection = engine.connect()
+    journal.DBSession.registry.clear()
+    journal.DBSession.configure(bind=connection)
+    journal.Base.metadata.bind = engine
+    request.addfinalizer(journal.Base.metadata.drop_all)
+    return connection
