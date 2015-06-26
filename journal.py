f5eb304ef34b94a47e4a7b3657f1d8f476a2d4ee
Jonathan Stallings <jonathanstallings@outlook.com>
2015-06-25 17:36:29 -0700

    Add new views and route

--- journal.py
+++ journal.py
@@ -4,8 +4,10 @@ import datetime
 import os
 
 from pyramid.config import Configurator
+from pyramid.httpexceptions import HTTPFound
 from pyramid.view import view_config
 import sqlalchemy as sa
+from sqlalchemy.exc import DBAPIError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 from waitress import serve
@@ -63,6 +65,22 @@ def list_view(request):
     return {'entries': entries}
 
 
+@view_config(route_name='add', request_method='POST')
+def add_entry(request):
+    title = request.params.get('title')
+    text = request.params.get('text')
+    Entry.write(title=title, text=text)
+    return HTTPFound(request.route_url('home'))
+
+
+@view_config(context=DBAPIError)
+def db_exception(context, request):
+    from pyramid.response import Response
+    response = Response(context.message)
+    response.status_int = 500
+    return response
+
+
 def main():
     """Create a configured wsgi app"""
     settings = {}
@@ -80,6 +98,7 @@ def main():
     config.include('pyramid_tm')
     config.include('pyramid_jinja2')
     config.add_route('home', '/')
+    config.add_route('add', '/add')
     config.scan()
     app = config.make_wsgi_app()
     return app
