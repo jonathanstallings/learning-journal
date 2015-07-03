158c6d0dcbdd6a451a9ca70243945f4c9ce2d13e
Jonathan Stallings <jonathanstallings@outlook.com>
2015-07-03 14:46:00 -0700

    Move edit_entry.feature

--- tests/test_journal_bdd.py
+++ tests/test_journal_bdd.py
@@ -2,13 +2,18 @@ import pytest
 from pytest_bdd import scenario, given, when, then
 
 
+@given('a learning journal home page')
+def home_page_exits():
+    pass
+
+
 @scenario('permalink.feature', 'Viewing an entry by permalink')
 def test_view_permalink():
     pass
 
 
 @given('I have an entry')
-def auth_user():
+def entry_exists():
     pass
 
 
@@ -24,4 +29,24 @@ def no_error_messages():
 
 @then('I should should see the entry detail')
 def see_entry_detail():
+    pass
+
+
+@scenario('permalink.feature', 'Entering an invalid permalink')
+def test_invalid_permalink():
+    pass
+
+
+@given('I do not have an entry')
+def entry_not_exists():
+    pass
+
+
+@when('I request a permalink')
+def request_permalink():
+    pass
+
+
+@then('I should receive a 404 error')
+def receive_404():
     pass
