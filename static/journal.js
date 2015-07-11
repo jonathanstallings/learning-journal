dfb25e437fb6e93d398d03a5e7f27b724a514620
Jonathan Stallings <jonathanstallings@outlook.com>
2015-07-10 21:53:52 -0700

    Complete AJAX Edit POST request

--- static/journal.js
+++ static/journal.js
@@ -3,8 +3,62 @@
 var taskEventHandler = function (e) {
     //Examine event and send to approriate handler
     var el = e.target;
-    var editButton = $('#edit-button');
-    var saveBuitton = $('#save-button');
+    var editButton = el.matches("#edit-button");
+    var saveButton = el.matches("#save-button");
+    var cancelButton = el.matches("#cancel-button");
+    
+    if (editButton) {
+        ajaxEditView(e);
+    } else if (saveButton) {
+        ajaxSaveFromEdit(e);
+    }
+};
+
+var ajaxEditView = function (e) {
+    //Send AJAX GET request for edit view
+    e.preventDefault();
+    var url = "/edit/" + $("article").data('entry-id');
+
+    $.ajax({
+        method: "GET",
+        url: url,
+        context: "#entry-edit"
+    }).done(function(response) {
+        $('#entry-detail').hide();
+        $('#entry-edit').show();
+        $('#title-edit').val(response.entry.title);
+        $('#text-edit').val(response.entry.text);
+    }).fail(function() {
+        alert( "error" );
+    });
+};
+
+
+var ajaxSaveFromEdit = function (e) {
+    //Send AJAX GET request for edit view
+    e.preventDefault();
+    var id = $("article").data('entry-id');
+    var title = $('#title-edit').val();
+    var text = $('#text-edit').val();
+    var url = "/edit/" + id;
+    
+
+    $.ajax({
+        method: "POST",
+        url: url,
+        data: {
+            id: id,
+            title: title,
+            text: text
+        }
+    }).done(function(response) {
+        $('#entry-edit').hide();
+        $('#entry-detail').show();
+        $('#title-detail').html(response.entry.title);
+        $('#text-detail').html(response.entry.markdown);
+    }).fail(function() {
+        alert( "error" );
+    });
 };
 
 
@@ -14,25 +68,30 @@ $('#navbar-toggle').on('click', function (e) {
     $('#navbar-collapse').toggleClass('collapse');
 });
 
-//
-$("#edit-button").on("click", function (e) {
-    e.preventDefault();
 
-    var url = "/edit/" + $("article").data('entry-id');
-    var main = $('main');
+// Event Listener for main section
+$('main').on("click", taskEventHandler);
 
-    $.ajax({
-        method: "GET",
-        url: url,
-        context: "#entry-edit"
-    })
-    .done(function(response) {
-        $('#entry-detail').hide();
-        $('#entry-edit').show();
-        $('#title').val(response.entry.title);
-        $('#text').val(response.entry.text);
-    })
-    .fail(function() {
-        alert( "error" );
-    });
-});
+
+// Event Listener for edit button
+// $("#edit-button").on("click", function (e) {
+//     e.preventDefault();
+
+//     var url = "/edit/" + $("article").data('entry-id');
+//     var main = $('main');
+
+//     $.ajax({
+//         method: "GET",
+//         url: url,
+//         context: "#entry-edit"
+//     })
+//     .done(function(response) {
+//         $('#entry-detail').hide();
+//         $('#entry-edit').show();
+//         $('#title').val(response.entry.title);
+//         $('#text').val(response.entry.text);
+//     })
+//     .fail(function() {
+//         alert( "error" );
+//     });
+// });
